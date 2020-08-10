06f4f248032153efd3c9dc596b3b269dcd5910f8
Тимур Белов <timurbl574@gmail.com>
2020-08-10 16:07:07 +0300

    generate data

---
+++ src/mock/trip-point.js
@@ -0,0 +1,82 @@
+import {TRIP_POINTS, TOWNS} from "../const";
+import {generateRandomListItem, getRandomInteger} from "../utils";
+import {generateAdditionalOption} from "./additional-option";
+
+const MAX_ADDITIONAL_OPTIONS = 5;
+
+const RANDOM_TEXT = [`Lorem ipsum dolor sit amet, consectetur adipiscing elit. `,
+  `Cras aliquet varius magna, non porta ligula feugiat eget. `,
+  `Fusce tristique felis at fermentum pharetra. `,
+  `Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. `,
+  `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. `,
+  `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. `,
+  `Sed sed nisi sed augue convallis suscipit in sed felis. `,
+  `Aliquam erat volutpat. `,
+  `Nunc fermentum tortor ac porta dapibus. `,
+  `In rutrum ac purus sit amet tempus. `];
+
+
+const generateDestinationInfo = () => {
+  const sentenceCount = getRandomInteger(0, 5);
+
+  let destinationInfo = ``;
+  for (let i = 0; i < sentenceCount; i++) {
+    let randomIndex = getRandomInteger(0, RANDOM_TEXT.length - 1);
+    destinationInfo = destinationInfo + RANDOM_TEXT[randomIndex];
+  }
+
+  return destinationInfo;
+};
+
+const generateDate = () => {
+  const maxDaysGap = 7;
+  const daysGap = getRandomInteger(1, maxDaysGap);
+  const currentDate = new Date();
+
+  currentDate.setHours(getRandomInteger(0, 23), getRandomInteger(0, 59));
+
+  currentDate.setDate(currentDate.getDate() + daysGap);
+
+  return new Date(currentDate);
+};
+
+const generateAdditionalOptions = () => {
+  const randomNumber = getRandomInteger(1, MAX_ADDITIONAL_OPTIONS);
+
+  let options = [];
+
+  for (let i = 0; i < randomNumber; i++) {
+    options.push(generateAdditionalOption());
+  }
+
+  return options;
+};
+
+const generateDestinationPhotos = () => {
+  const numberOfPhotos = getRandomInteger(1, 6);
+  let eventPhotos = ``
+
+  for (let i = 0; i< numberOfPhotos; i++) {
+    eventPhotos = eventPhotos + `<img class="event__photo" src="http://picsum.photos/248/152?r=${Math.random()}" alt="Event photo">`
+  }
+
+  return eventPhotos;
+}
+
+export const generateTripPoint = () => {
+  const destinationInfo = generateDestinationInfo();
+  let destinationPhotos = [];
+  if (destinationInfo !== ``) {
+    destinationPhotos = generateDestinationPhotos();
+  }
+  return {
+    tripPointType: generateRandomListItem(TRIP_POINTS),
+    destination: generateRandomListItem(TOWNS),
+    destinationInfo,
+    destinationPhotos,
+    startDate: generateDate(),
+    endDate: generateDate(),
+    cost: getRandomInteger(100, 10000),
+    additionalOptions: generateAdditionalOptions(),
+  };
+};
