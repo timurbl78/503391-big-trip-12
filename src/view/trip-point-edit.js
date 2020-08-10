26e79eb7ea86a8c54aa619dff1bce94232746b43
Тимур Белов <timurbl574@gmail.com>
2020-08-10 16:09:16 +0300

    add info about tripPoint

--- src/view/trip-point-edit.js
+++ src/view/trip-point-edit.js
@@ -1,4 +1,57 @@
-export const createTripEditTemplate = () => {
+import {TRIP_SOME} from "../const";
+import {getRandomInteger} from "../utils";
+
+export const createDestinationBlock = (tripPoint) => {
+  return (
+    `<section class="event__section  event__section--destination">
+    <h3 class="event__section-title  event__section-title--destination">Destination</h3>
+    <p class="event__destination-description">${tripPoint.destinationInfo}</p>
+
+    <div class="event__photos-container">
+      <div class="event__photos-tape">
+        ${tripPoint.destinationPhotos}
+      </div>
+    </div>
+  </section>`)
+}
+
+const generateAdditionalOptions = (tripPoint) => {
+  let options = ``;
+  for (let i = 0; i < tripPoint.additionalOptions.length; i++) {
+    const option = tripPoint.additionalOptions[i];
+    options = options + `<div class="event__offer-selector">
+      <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" ${getRandomInteger(0, 1) ? `checked` : ``}>
+      <label class="event__offer-label" for="event-offer-luggage-1">
+        <span class="event__offer-title">${option.name}</span>
+        &plus;
+        &euro;&nbsp;<span class="event__offer-price">${option.cost}</span>
+      </label>
+    </div>`;
+  }
+
+  return options;
+}
+
+export const createTripPointEditTemplate = (tripPoint) => {
+  const startMinutes = tripPoint.startDate.getMinutes();
+  const startHours = tripPoint.startDate.getHours();
+  const startDay = tripPoint.startDate.getDay();
+  const startMonth = tripPoint.startDate.getMonth();
+  const startYear = tripPoint.startDate.getFullYear() % 100;
+
+  const endMinutes = tripPoint.endDate.getMinutes();
+  const endHours = tripPoint.endDate.getHours();
+  const endDay = tripPoint.endDate.getDay();
+  const endMonth = tripPoint.endDate.getMonth();
+  const endYear = tripPoint.endDate.getFullYear() % 100;
+
+
+  const additionalOptions = generateAdditionalOptions(tripPoint);
+  let destinationInfoBlock = ``;
+  if (tripPoint.destinationInfo !== ``) {
+    destinationInfoBlock = createDestinationBlock(tripPoint);
+  }
+
   return (
     `<li class="trip-events__item">
       <form class="event  event--edit" action="#" method="post">
@@ -6,7 +59,7 @@ export const createTripEditTemplate = () => {
           <div class="event__type-wrapper">
             <label class="event__type  event__type-btn" for="event-type-toggle-1">
               <span class="visually-hidden">Choose event type</span>
-              <img class="event__type-icon" src="img/icons/flight.png" alt="Event type icon" width="17" height="17">
+              <img class="event__type-icon" src="img/icons/${tripPoint.tripPointType.toLowerCase()}.png" alt="Event type icon" width="17" height="17">
             </label>
             <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
 
@@ -73,9 +126,9 @@ export const createTripEditTemplate = () => {
 
           <div class="event__field-group  event__field-group--destination">
             <label class="event__label  event__type-output" for="event-destination-1">
-              Flight to
+              ${tripPoint.tripPointType} ${TRIP_SOME.get(tripPoint.tripPointType)}
             </label>
-            <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="Chamonix" list="destination-list-1">
+            <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${tripPoint.destination}" list="destination-list-1">
             <datalist id="destination-list-1">
               <option value="Amsterdam"></option>
               <option value="Geneva"></option>
@@ -87,12 +140,12 @@ export const createTripEditTemplate = () => {
             <label class="visually-hidden" for="event-start-time-1">
               From
             </label>
-            <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="18/03/19 12:25">
+            <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${startDay}/${startMonth}/${startYear} ${startHours}:${startMinutes}">
             —
             <label class="visually-hidden" for="event-end-time-1">
               To
             </label>
-            <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="18/03/19 13:35">
+            <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${endDay}/${endMonth}/${endYear} ${endHours}:${endMinutes}">
           </div>
 
           <div class="event__field-group  event__field-group--price">
@@ -100,7 +153,7 @@ export const createTripEditTemplate = () => {
               <span class="visually-hidden">Price</span>
               €
             </label>
-            <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="160">
+            <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${tripPoint.cost}">
           </div>
 
           <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
@@ -124,52 +177,10 @@ export const createTripEditTemplate = () => {
             <h3 class="event__section-title  event__section-title--offers">Offers</h3>
 
             <div class="event__available-offers">
-              <div class="event__offer-selector">
-                <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked="">
-                <label class="event__offer-label" for="event-offer-luggage-1">
-                  <span class="event__offer-title">Add luggage</span>
-                  +
-                  €&nbsp;<span class="event__offer-price">30</span>
-                </label>
-              </div>
-
-              <div class="event__offer-selector">
-                <input class="event__offer-checkbox  visually-hidden" id="event-offer-comfort-1" type="checkbox" name="event-offer-comfort" checked="">
-                <label class="event__offer-label" for="event-offer-comfort-1">
-                  <span class="event__offer-title">Switch to comfort class</span>
-                  +
-                  €&nbsp;<span class="event__offer-price">100</span>
-                </label>
-              </div>
-
-              <div class="event__offer-selector">
-                <input class="event__offer-checkbox  visually-hidden" id="event-offer-meal-1" type="checkbox" name="event-offer-meal">
-                <label class="event__offer-label" for="event-offer-meal-1">
-                  <span class="event__offer-title">Add meal</span>
-                  +
-                  €&nbsp;<span class="event__offer-price">15</span>
-                </label>
-              </div>
-
-              <div class="event__offer-selector">
-                <input class="event__offer-checkbox  visually-hidden" id="event-offer-seats-1" type="checkbox" name="event-offer-seats">
-                <label class="event__offer-label" for="event-offer-seats-1">
-                  <span class="event__offer-title">Choose seats</span>
-                  +
-                  €&nbsp;<span class="event__offer-price">5</span>
-                </label>
-              </div>
-
-              <div class="event__offer-selector">
-                <input class="event__offer-checkbox  visually-hidden" id="event-offer-train-1" type="checkbox" name="event-offer-train">
-                <label class="event__offer-label" for="event-offer-train-1">
-                  <span class="event__offer-title">Travel by train</span>
-                  +
-                  €&nbsp;<span class="event__offer-price">40</span>
-                </label>
-              </div>
+              ${additionalOptions}
             </div>
           </section>
+          ${destinationInfoBlock}
         </section>
       </form>
     </li>`
