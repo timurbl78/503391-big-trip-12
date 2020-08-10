5e32dfe0c5740c91729fd2eb1ec8a7a428bf0fd1
Тимур Белов <timurbl574@gmail.com>
2020-08-10 16:05:27 +0300

    updated const.js

---
+++ src/const.js
@@ -0,0 +1,5 @@
+export const TRIP_POINTS = [`Taxi`, `Bus`, `Train`, `Ship`, `Transport`, `Drive`, `Flight`, `Check-in`, `Sightseeing`, `Restaurant`];
+export const TOWNS = [`Moscow`, `Dubai`, `Berlin`, `New York`, `Colombo`, `Sochi`];
+export const TRIP_SOME = new Map([[`Taxi`, `to`], [`Bus`, `to`], [`Train`, `to`], [`Ship`, `to`], [`Transport`, `to`],
+  [`Drive`, `to`], [`Flight`, `to`], [`Check-in`, `in`], [`Sightseeing`, `in`], [`Restaurant`, `in`]]);
+export const ADDITIONAL_OPTIONS = [`Order Uber`, `Add meal`, `Add luggage`, `Travel by train`];
