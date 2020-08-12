a08b89f8aa03641667be385153393f13b6f26bd5
Тимур Белов <timurbl574@gmail.com>
2020-08-13 02:23:37 +0300

    rewrite render function

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,8 @@
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
 export const getRandomInteger = (a = 0, b = 1) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -10,3 +15,21 @@ export const generateRandomListItem = (list) => {
 
   return list[randomIndex];
 }
+
+export const createElement = (template) => {
+  const container = document.createElement(`div`);
+  container.innerHTML = template;
+
+  return container.firstChild;
+};
+
+export const render = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
