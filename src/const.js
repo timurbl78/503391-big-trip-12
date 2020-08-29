268210d686997fd0369e8c0bf3a2ae80ae100e0f
Тимур Белов <timurbl574@gmail.com>
2020-08-29 19:24:37 +0300

    SortType const

--- src/const.js
+++ src/const.js
@@ -3,3 +3,8 @@ export const TRIP_POINTS_MAP = new Map([[`taxi`, `to`], [`bus`, `to`], [`train`,
 export const ADDITIONAL_OPTIONS = [`Order Uber`, `Add meal`, `Add luggage`, `Travel by train`];
 export const TRIP_POINT_TRANSFER_TYPES = [`taxi`, `bus`, `train`, `ship`, `transport`, `drive`, `flight`];
 export const TRIP_POINT_ACTIVITIES_TYPE = [`check-in`, `sightseeing`, `restaurant`];
+export const SortType = {
+  EVENT: `event`,
+  TIME: `time`,
+  PRICE: `price`
+};
