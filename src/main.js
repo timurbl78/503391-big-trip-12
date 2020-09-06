b14d3384606153a59b679eb9909c45906a0b6cda
Тимур Белов <timurbl574@gmail.com>
2020-09-06 17:06:51 +0300

    changed events count

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import BoardPresenter from "./presenter/board";
 import {generateTripPoint} from "./mock/trip-point";
 
-const EVENTS_COUNT = 4;
+const EVENTS_COUNT = 20;
 
 const tripPoints = new Array(EVENTS_COUNT).fill().map(generateTripPoint);
 
