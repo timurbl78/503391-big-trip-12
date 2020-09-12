bb1a76e9da46e89e2b20b439c26190068d067a1d
Тимур Белов <timurbl574@gmail.com>
2020-09-12 22:39:54 +0300

    api class

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import BoardPresenter from "./presenter/board";
 import {generateTripPoint} from "./mock/trip-point";
+import Api from "./api";
 
 const EVENTS_COUNT = 20;
+
+const AUTHORIZATION = `Basic hS2sd3dfSf4dEsa2j`;
+const END_POINT = `https://12.ecmascript.pages.academy/big-trip`;
+
+const api = new Api(END_POINT, AUTHORIZATION);
+
+api.getPoints().then((points) => {
+
+});
 
 const tripPoints = new Array(EVENTS_COUNT).fill().map(generateTripPoint);
 
