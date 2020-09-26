113accbe6defcbc2dd5eb16d23cb661163f4a6ee
Тимур Белов <timurbl574@gmail.com>
2020-09-26 13:07:26 +0300

    serviceWorker & store class

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import BoardPresenter from "./presenter/board";
-import Api from "./api";
+import Api from "./api/index";
 
 const AUTHORIZATION = `Basic hS2sd3dfSf4dEsa2j`;
 const END_POINT = `https://12.ecmascript.pages.academy/big-trip`;
@@ -9,3 +9,13 @@ const api = new Api(END_POINT, AUTHORIZATION);
 const boardPresenter = new BoardPresenter(api);
 
 boardPresenter.init();
+
+window.addEventListener(`load`, () => {
+  navigator.serviceWorker.register(`/sw.js`)
+    .then(() => {
+      console.log(`ServiceWorker available`); // eslint-disable-line
+    }).catch(() => {
+    console.error(`ServiceWorker isn't available`); // eslint-disable-line
+    });
+});
+
